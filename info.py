9c60566018a881f4cc9305a4d0c20cd05c2c8f22
DeveloperJaat <97823189+DeveloperJaat@users.noreply.github.com>
2025-04-04 16:43:23 +0530

    1

--- info.py
+++ info.py
@@ -97,7 +97,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'easysky.in/')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'e8982e891252fdb2a596420fa8148ee7d742b5b1')
