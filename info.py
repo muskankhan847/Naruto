e289a0d184c6bf33299e36b8e2a847e1a3fac688
j13179356 <j13179356@gmail.com>
2025-04-07 09:39:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,10 +72,10 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+QtqcYcCt_LJiZjE1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/madyyyyyyyl')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/showtimemovierequestgrouppp')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/showtimelatest')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+QtqcYcCt_LJiZjE1') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'showtimemovierequestgrouppp') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', False))
